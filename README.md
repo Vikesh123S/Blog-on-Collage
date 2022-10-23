4b53d46fc9a9fd4f76ac511eb90142ed7b0914e9
Vikesh <45270421+vikeshdas@users.noreply.github.com>
2022-10-24 01:18:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# CollegeReview
+
+“User can find all details of each collage like what is the fee of the collage ,review of the other people on this collage ,location of the collage.all these details will be posted by some other user .This applicaiton helps students to find best collage. 
+
+### This is home page here all collages are listed ,these post are shared by some other user
+<img src="images/1.png" width="100%" align="top-left" alt="" />
+
+### User can view details of a post 
+<img src="images/2.png" width="100%" align="top-left" alt="" />
+
+### if a user wants to update a post can do .only author of the post can edit the post.
+<img src="images/3.png" width="100%" align="top-left" alt="" />
+
+### Logedin User can write review on the perticular post
+<img src="images/6.png" width="100%" align="top-left" alt="" />
+
+### This is register page
+<img src="images/4.png" width="100%" align="top-left" alt="" />
+
+### This is Login page
+<img src="images/5.png" width="100%" align="top-left" alt="" />
+
+ 
---
+++ middleware.js
@@ -0,0 +1,58 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Post = require('./models/post');
+const Review = require('./models/review');
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    console.log("User logedin");
+    next();
+}
+
+module.exports.validatePost = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    console.log(req.body);
+    if (error) {
+        console.log(error);
+        console.log("error inside validatePost")
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        console.log("calidated post");
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (!post.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/posts/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/posts/${id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
